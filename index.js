794675077b0ae6a10a12eb8c2ca8432449045e55
Nir Galon <nir@galons.io>
2025-03-08 17:29:33 +0200

    Learning about rebase

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("This is a new line")
 console.log("we will undo this change later on")
 console.log("another line")
 console.log("Bob made that change in chapter 3");
+console.log("We are learning about rebase");
