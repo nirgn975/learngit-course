061f7ba0862b699f797582c8d651ed5e84a18101
Nir Galon <nir@galons.io>
2025-03-09 09:49:01 +0200

    A change on main

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log("we will undo this change later on")
 console.log("another line")
 console.log("Bob made that change in chapter 3");
 console.log("We are learning about rebase");
+console.log("A change on main branch");
