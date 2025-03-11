5829c5f46d2dc6fb575870735e0d31d669dc5d5e
Nir Galon <nir@galons.io>
2025-03-11 16:35:47 +0200

    Remove older files and add a new gitignore file

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log("This is a new line")
-console.log("we will undo this change later on")
-console.log("another line")
-console.log("Bob made that change in chapter 3");
-console.log("We are learning about rebase");
-console.log("A change on main branch");
